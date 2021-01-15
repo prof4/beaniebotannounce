18489cd917731b325ecd024116469ddea6b19db0
prof4 <52273992+prof4@users.noreply.github.com>
2021-01-15 08:56:53 +0100

    Update announce.js

--- commands/commands/announce.js
+++ commands/commands/announce.js
@@ -1,5 +1,5 @@
 const language = require('@util/language')
-const channelId = '734698971175321640'
+const channelId = '798041957825118238'
 module.exports = {
   commands: ['announce', 'a'],
   permissionError: 'You need admin permissions to run this command',
